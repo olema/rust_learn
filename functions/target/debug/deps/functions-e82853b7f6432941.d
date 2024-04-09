1741ea2ff35f7d49540563171536d92e8d10e609
Oleg Matushkin <matushkin.oleg@gmail.com>
2024-04-09 16:07:45 +0300

    add functions

---
+++ functions/target/debug/deps/functions-e82853b7f6432941.d
@@ -0,0 +1,5 @@
+/home/ollo/progs/rust_learn/functions/target/debug/deps/functions-e82853b7f6432941: src/main.rs
+
+/home/ollo/progs/rust_learn/functions/target/debug/deps/functions-e82853b7f6432941.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/ollo/progs/rust_learn/functions/target/debug/functions: /home/ollo/progs/rust_learn/functions/src/main.rs
