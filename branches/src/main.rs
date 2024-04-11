f0b97c45fc00a2292dcca5efa0dcb8bb29475077
Oleg Matushkin <matushkin.oleg@gmail.com>
2024-04-11 08:06:13 +0300

    branches end

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -18,7 +18,62 @@ fn main() {
     }
 
     let condition = true;
-    let number = if condition { 5 } else { 6 };
+    let mut number = if condition { 5 } else { 6 };
 
     println!("The value of number is: {number}");
+    
+    // loop
+    loop {
+        println!("{number}. again!");
+        if number > 10 { break};
+        number += 1;
+    }
+
+    // returning value from loop
+    let result = loop {
+        number += 1;
+        if number > 15 {
+            break number * 2; // break return value
+        }
+    };
+    println!("result loop = {result}");
+
+    // loop lables
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up; // exit from outer loop
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    // while 
+    number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    // for statement through the elements of collection
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a {
+        println!("the value is: {element}");
+    }
 }
