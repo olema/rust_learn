dbac95a762e8b81979a1e7ab25b442cbc1470768
Oleg Matushkin <matushkin.oleg@gmail.com>
2024-04-11 07:41:36 +0300

    branches

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/ollo/progs/rust_learn/branches/target/debug/branches: /home/ollo/progs/rust_learn/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-06c5ac9f0d50e133.d
@@ -0,0 +1,5 @@
+/home/ollo/progs/rust_learn/branches/target/debug/deps/branches-06c5ac9f0d50e133: src/main.rs
+
+/home/ollo/progs/rust_learn/branches/target/debug/deps/branches-06c5ac9f0d50e133.d: src/main.rs
+
+src/main.rs:
