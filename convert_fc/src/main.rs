667a09d318c581c2a5fe1cf0ec31d4f0246ceb8c
Oleg Matushkin <matushkin.oleg@gmail.com>
2024-04-16 08:03:52 +0300

    convert_fc end

--- convert_fc/src/main.rs
+++ convert_fc/src/main.rs
@@ -3,33 +3,61 @@ use std::io;
 
 fn main() {
     println!("Эта программа конвертирует цельсии в фаренгейты и наоборот.");
-    println!("Задайте направление конвертации (1 - по умолчанию):");
-    println!("  1 - фаренгейты в цельсии;");
-    println!("  2 - цельсии в фаренгейты;");
-    println!("  3 - выход.");
+    println!("Задайте направление конвертации (0 - по умолчанию):");
+    println!("  0 - фаренгейты в цельсии;");
+    println!("  1 - цельсии в фаренгейты;");
+    println!("  2 - выход.");
 
     loop {
-        println!("1/2/3?");
+        println!("0: Ф->Ц, 1: Ц->Ф, 2: выход?");
 
         let mut cf = String::new();
-        let mut znach = String::new();
+//        let arr = ["",
 
         io::stdin().read_line(&mut cf).expect("Filed to read line");
 
-        let cf: u8 = match cf.trim().parse() {
+        let cf: u64 = match cf.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
         
-        if cf == 1 {
-            println!("cf = {cf}");
+        if cf == 0 {
+            // фарен -> цельс
+            println!("Фаренгейты -> Цельсии");
+            println!("Введите температуру в фаренгейтах:");
+            let znach = input_znach();
+            println!("Вы ввели: {znach}. Температура в цельсиях: {}", convert_fc(znach, cf));
+        } else if cf == 1 {
+            println!("Цельсии -> Фаренгейты");
+            println!("Введите температуру в цельсиях:");
+            let znach = input_znach();
+            println!("Вы ввели: {znach}. Температура в фаренгейтах: {}", convert_fc(znach, cf));
         } else if cf == 2 {
-            println!("cf = {cf}");
-        } else if cf == 3 {
             println!("Goodby!");
             break
         }
     }
 }
 
-fn 
+fn input_znach() -> i64 {
+    let mut znach = String::new();
+    io::stdin().read_line(&mut znach).expect("Filed to read line");
+    let znach: i64 = match znach.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    };
+    znach
+}
+
+fn convert_fc(x:i64, n: u64) -> i64 {
+//    (Фаренгейт — 32) : 1,8 = Цельсий Пример: (50°F - 32) : 1,8 = 10°C
+//    Цельсий х 1,8 + 32 = Фаренгейт Пример: 10°C x 1,8 + 32 = 50°F
+    let mut res: i64 = 0;
+    if n == 0 {
+        res = (x - 32)/2;
+    } else {
+        res = (x * 2) + 32;
+    }
+    res
+}
+
